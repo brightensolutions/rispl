bf33192509d62d80127ad4bd9c0ca108f6d07002
Devbrighten <developer.brightensolutions@gmail.com>
2025-10-06 17:25:10 +0530

    changes

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -226,9 +226,8 @@ export function Footer() {
                   >
                     <MapPin className="w-5 h-5 mt-1 group-hover:text-gold transition-colors" />
                     <span>
-                      704, 7th Floor, Tower B, D Wing, Swastik Universal
-                      Building, Near Valentine Cinema, Piplod, Surat, Gujarat
-                      395007, India
+                      PLOT 47-50, BHATPORE GIDC, NR. GAIL COLONY, BHATPORE,
+                      SURAT-394510.
                     </span>
                   </a>
                 </li>

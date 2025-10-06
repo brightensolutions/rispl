ba027a02c11db2cc3dd34e0d697261406b0dbe0d
Devbrighten <developer.brightensolutions@gmail.com>
2025-10-06 17:47:18 +0530

    address

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -224,7 +224,7 @@ export function Footer() {
                     href="#"
                     className="text-gray-300 hover:text-gold flex items-start gap-3 group"
                   >
-                    <MapPin className="w-5 h-5 mt-1 group-hover:text-yellow-500 transition-colors" />
+                    <MapPin className="w-5 h-5 mt-1 group-hover:text-gold transition-colors" />
                     <span>
                       704, 7th Floor, Tower B, D Wing, Swastik Universal
                       Building, Near Valentine Cinema, Piplod, Surat, Gujarat
